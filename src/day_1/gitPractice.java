74e6d87a70cb0db2ccdf984a8deda61831c7224a
gulmaqbool <64177635+gulmaqbool@users.noreply.github.com>
2020-05-08 15:07:07 -0500

    Update gitPractice.java

--- src/day_1/gitPractice.java
+++ src/day_1/gitPractice.java
@@ -4,6 +4,15 @@ public class gitPractice {
     public static void main(String[] args) {
         System.out.println("Second practice of Git project");
 
+        
+        
+        //Hello
+        //my name is Zareen
+        //this is my github 
+        //I'm trying to practice it
+        //Thank you so much.
+        
+        
     }
 
 
