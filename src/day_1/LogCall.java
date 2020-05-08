c9aace72bbd0ea4a64730343ebf825bf035a86a5
gulmaqbool <gulmaqbool@yahoo.com>
2020-05-08 15:11:35 -0500

    add LogCall.java

---
+++ src/day_1/LogCall.java
@@ -0,0 +1,12 @@
+package day_1;
+
+public class LogCall {
+    public static void main(String[] args) {
+
+        System.out.println("I previously made this class");
+        System.out.println("Then I deleted it");
+        System.out.println("Now I'm writing in it again");
+        System.out.println("Thank you");
+
+    }
+}
